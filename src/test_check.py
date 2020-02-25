94dea311cee8a2dc3b73db87af833fa39545e3f9
Joanna Stadnik <joanna.stadnik@wegotpop.com>
2020-02-25 11:37:50 +0000

    updated test

--- src/test_check.py
+++ src/test_check.py
@@ -1,8 +1,13 @@
+import pytest
 import check
 
-def test_process_line():
-    line = "boto3==1.2.4"
-    assert check.process_line(line) == ['boto3', "1.2.4"]
-    line = "# I am a comment"
-    assert check.process_line(line) is None
-    
+
+@pytest.mark.parametrize(
+    "line, expected", [("boto3==1.2.4", ["boto3", "1.2.4"]), ("# I am a comment", None)]
+)
+def test_process_line(line, expected):
+    assert check.process_line(line) == expected
+
+
+def test_process_file():
+    pass
